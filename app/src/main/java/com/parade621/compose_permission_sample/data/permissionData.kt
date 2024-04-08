cb20d95a413b77b60559c2271b17d3112c3bb4c8
몰름보반장 <parade621@naver.com>
2024-04-09 00:06:35 +0900

    add data class

--- app/src/main/java/com/parade621/compose_permission_sample/data/permissionData.kt
+++ app/src/main/java/com/parade621/compose_permission_sample/data/permissionData.kt
@@ -1,3 +1,6 @@
 package com.parade621.compose_permission_sample.data
 
-data class permissionData()
+data class PermissionItem(
+    val permissionText: String,
+    var isChecked: Boolean = false
+)
